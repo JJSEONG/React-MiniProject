fe402a0b169dce724074979809f7513258f46053
JJSEONG <hans3238@naver.com>
2022-07-24 01:46:48 +0900

    항해99 : MiniProject AddPost page 완료 및 Detail 틀 작업

--- src/App.js
+++ src/App.js
@@ -4,27 +4,18 @@ import { Routes, Route } from 'react-router-dom'
 import Login from './components/Login';
 import Main from './components/Main';
 import Signup from './components/Signup';
-<<<<<<< HEAD
 import AddPost from './components/AddPost';
-=======
 import Detail from './components/Detail';
->>>>>>> 5ecd0220e1ae7211e59341e68caad70d259083f0
 
 function App() {
   return (
     <div className="App">
       <Routes>
-<<<<<<< HEAD
         <Route path="/" element = {<Main /> } />
         <Route path="/login" element ={<Login />} />
         <Route path="/login/signup" element={<Signup />} />
         <Route path="/newBoards" element = {<AddPost />} />
-=======
-        <Route path="/selecthing" element = {<Main /> }/>
-        <Route path="/login" element ={<Login/>}/>
-        <Route path="/login/signup" element={<Signup/>}/>
         <Route path="/boards/details" element={<Detail/>}/>
->>>>>>> 5ecd0220e1ae7211e59341e68caad70d259083f0
       </Routes>
     </div>
   );
